0a12fdc222110e841779bbb762ca7203e08ebda7
jirbthagoras <hansjabriel@gmail.com>
2026-01-02 23:57:35 +0700

    [+]System/schema: added a relation to make query more easy

--- main-service/src/db/schema/logfiles.schema.ts
+++ main-service/src/db/schema/logfiles.schema.ts
@@ -1,12 +1,23 @@
-import { InferInsertModel, InferSelectModel } from "drizzle-orm";
+import { InferInsertModel, InferSelectModel, relations } from "drizzle-orm";
 import { pgTable } from "drizzle-orm/pg-core";
+import { userSchema } from "./user.schema";
 
 export const logFileSchema = pgTable("logfiles", (t) => ({
   id: t.integer().primaryKey().generatedAlwaysAsIdentity(),
-  userId: t.integer().notNull(),
+  userId: t
+    .integer()
+    .notNull()
+    .references(() => userSchema.id),
   name: t.varchar().notNull(),
   description: t.text().notNull(),
   createdAt: t.timestamp().defaultNow(),
+}));
+
+export const logFileRelations = relations(logFileSchema, ({ one }) => ({
+  user: one(userSchema, {
+    fields: [logFileSchema.userId],
+    references: [userSchema.id],
+  }),
 }));
 
 export type LogFile = InferSelectModel<typeof logFileSchema>;
--- main-service/src/db/schema/user.schema.ts
+++ main-service/src/db/schema/user.schema.ts
@@ -1,5 +1,6 @@
-import { InferInsertModel, InferSelectModel } from "drizzle-orm";
+import { InferInsertModel, InferSelectModel, relations } from "drizzle-orm";
 import { pgTable } from "drizzle-orm/pg-core";
+import { logFileSchema } from "./logfiles.schema";
 
 export const userSchema = pgTable("users", (t) => ({
   id: t.integer().primaryKey().generatedAlwaysAsIdentity(),
@@ -8,6 +9,11 @@ export const userSchema = pgTable("users", (t) => ({
   password: t.varchar().notNull(),
   verified: t.boolean().default(false),
   verifiedAt: t.timestamp(),
+}));
+
+// userRelation needed so that I am not refetching the logfiles again soon.
+export const userRelations = relations(userSchema, ({ many }) => ({
+  logfiles: many(logFileSchema),
 }));
 
 export type User = InferSelectModel<typeof userSchema>;
