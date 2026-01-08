c0aeb03ce264af36a8630f015fd769cde31b6d48
jirbthagoras <hansjabriel@gmail.com>
2026-01-08 21:44:42 +0700

    [+]feat/auth: setted up an auth plugin

---
+++ main-service/src/plugins/auth.plugin.ts
@@ -0,0 +1,35 @@
+import cookie from "@elysiajs/cookie";
+import jwt from "@elysiajs/jwt";
+import Elysia from "elysia";
+
+type AuthUser = {
+  id: string;
+};
+
+export const authPlugin = new Elysia()
+  .use(cookie())
+  .use(
+    jwt({
+      name: "jwt",
+      secret: process.env.JWT_SECRET!,
+    }),
+  )
+  .derive({ as: "global" }, async ({ jwt, cookie }) => {
+    const raw = cookie.auth_token?.value;
+
+    if (!raw) return { user: null as AuthUser | null };
+
+    if (typeof raw !== "string") return { user: null as AuthUser | null };
+
+    const payload = await jwt.verify(raw);
+
+    return { user: payload as AuthUser };
+  });
+
+export const requireAuth = new Elysia()
+  .use(authPlugin)
+  .onBeforeHandle(({ user }) => {
+    if (!user) {
+      return {};
+    }
+  });
